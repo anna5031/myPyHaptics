aad4a744a9d8e36a4cd7d0f0729d1ba1764f9ec4
sohyeonYu <anna5031@kaist.ac.kr>
2026-03-20 18:30:16 +0900

    bHaptics Bridge implement

--- src/bhaptics_bridge.py
+++ src/bhaptics_bridge.py
@@ -1,11 +1,26 @@
-class BHapticsBridge:
-    def __init__(self):
-        pass
+import bhaptics_python
+import asyncio
 
-    def play_dot(self):
-        pass
+TACT_SUIT = 0
+MOTORS_COUNT = 32
+
+class BHapticsBridge:
+    def __init__(self, app_id="698945534e2e268ff3a49d5b", api_key="BnlVMoYwk8ikSahocPx5", app_name="Make Lab"):
+        self.app_id = app_id
+        self.api_key = api_key
+        self.app_name = app_name
+
+    async def connect(self):
+        result = await bhaptics_python.registry_and_initialize(self.app_id, self.api_key, self.app_name)
+        return result
+
+    async def play_dot(self, strength):
+        device_type = TACT_SUIT
+        motors_count = MOTORS_COUNT
+        await bhaptics_python.play_dot(device_type, strength, [strength] * motors_count)
 
 
 if __name__ == "__main__":
     bridge = BHapticsBridge()
-    bridge.play_dot()
+    result = asyncio.run(bridge.connect())
+    asyncio.run(bridge.play_dot(100))

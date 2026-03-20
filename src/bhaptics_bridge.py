557dd193f47beec5cd8e9cb8bb649f6354d73042
Cotidie <daily142857@gmail.com>
2026-03-20 17:39:10 +0900

    bHaptics Bridge Skeleton

---
+++ src/bhaptics_bridge.py
@@ -0,0 +1,11 @@
+class BHapticsBridge:
+    def __init__(self):
+        pass
+
+    def play_dot(self):
+        pass
+
+
+if __name__ == "__main__":
+    bridge = BHapticsBridge()
+    bridge.play_dot()
